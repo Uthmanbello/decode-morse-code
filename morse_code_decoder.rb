a58452af35523bd22f9a00c20e382a9bed46db8b
DarkxDev <bryanfernandohurtado@gmail.com>
2023-04-05 16:03:31 -0500

    Updated code to use class and a hash

---
+++ morse_code_decoder.rb
@@ -0,0 +1,42 @@
+class MorseCodeDecoder
+  MORSE_CODE = {
+    ".-" => "A",
+     "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+    "." => "E",
+    "..-." => "F",
+    "--." => "G",
+    "...." => "H",
+    ".." => "I",
+    ".---" => "J",
+    "-.-" => "K",
+    ".-.." => "L",
+    "--" => "M",
+    "-." => "N",
+    "---" => "O",
+    ".--." => "P",
+    "--.-" => "Q",
+    ".-." => "R",
+    "..." => "S",
+    "-" => "T",
+    "..-" => "U",
+    "...-" => "V",
+    ".--" => "W",
+    "-..-" => "X",
+    "-.--" => "Y",
+    "--.." => "Z"
+  }
+
+  def self.decode_char(code)
+    MORSE_CODE[code]
+  end
+
+  def self.decode_word(code)
+    code.split(" ").map { |c| decode_char(c) }.join("")
+  end
+
+  def self.decode_sentence(code)
+    code.split("   ").map { |w| decode_word(w) }.join(" ")
+  end
+end
